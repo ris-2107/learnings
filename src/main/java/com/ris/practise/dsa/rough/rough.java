a1a229ce3d66e8543f40d48c7d324e9a345c7d3d
ris2107 <rishabh210798@gmail.com>
2024-04-18 00:48:46 +0530

    Initial

---
+++ src/main/java/com/ris/practise/dsa/rough/rough.java
@@ -0,0 +1,14 @@
+package com.ris.practise.dsa.rough;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class rough {
+    public static void main(String args []){
+        System.out.println(testingStrFnc());
+    }
+
+    public static String testingStrFnc(){
+        return "Success";
+    }
+}
