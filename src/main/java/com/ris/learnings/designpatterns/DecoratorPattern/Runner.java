a7a89421b8dd1b94a353850c0626e85671e62a28
ris2107 <rishabh210798@gmail.com>
2024-04-20 17:19:41 +0530

    20 Apr, 2024

--- src/main/java/com/ris/learnings/designpatterns/DecoratorPattern/Runner.java
+++ src/main/java/com/ris/learnings/designpatterns/DecoratorPattern/Runner.java
@@ -7,8 +7,8 @@ import com.ris.learnings.designpatterns.DecoratorPattern.Pizzas.Margherita;
 
 public class Runner {
     public static void main(String[] args) {
-        BasePizza margherita = new Margherita();
-        BasePizza margheritaWithExtraCheese = new ExtraCheese(margherita);
+        BasePizza baseMargherita = new Margherita();
+        BasePizza margheritaWithExtraCheese = new ExtraCheese(baseMargherita);
         BasePizza margheritaWithExtraCheeseWithMushroom = new Mushroom(margheritaWithExtraCheese);
 
         System.out.println(margheritaWithExtraCheese.getClass().getName() + "'s cost is : " + margheritaWithExtraCheese.cost());
