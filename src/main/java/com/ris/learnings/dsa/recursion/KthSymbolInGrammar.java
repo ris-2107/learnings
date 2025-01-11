4c452f35c7daf178b0c35bbc1a744be73a4287dd
Rishabh Sinha <rsinha@tekion.com>
2025-01-12 03:27:06 +0530

    refact [0.1]

---
+++ src/main/java/com/ris/learnings/dsa/recursion/KthSymbolInGrammar.java
@@ -0,0 +1,24 @@
+package com.ris.learnings.dsa.recursion;
+
+// leet code question
+public class KthSymbolInGrammar {
+  public static void main(String[] args) {}
+
+  private static int grammar(int n, int k) {
+
+    // base condition:
+    if (n == 1 && k == 1) {
+      return 0;
+    }
+    // parent position:
+    int parent = (k + 1) / 2;
+
+    int parentValue = grammar(n - 1, parent);
+
+    if (k % 2 == 0) {
+      return 1 - parentValue;
+    } else {
+      return parentValue;
+    }
+  }
+}
