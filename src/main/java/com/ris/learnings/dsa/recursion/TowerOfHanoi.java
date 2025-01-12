6889d022072fc45630f3cbb359516b0e988b7a2f
Rishabh Sinha <rsinha@tekion.com>
2025-01-12 21:54:27 +0530

    12-jan-2025[0.0]

--- src/main/java/com/ris/learnings/dsa/recursion/TowerOfHanoi.java
+++ src/main/java/com/ris/learnings/dsa/recursion/TowerOfHanoi.java
@@ -6,10 +6,10 @@ import lombok.extern.slf4j.Slf4j;
 public class TowerOfHanoi {
 
   public static void main(String[] args) {
-    int n = 2;
-    int[] c = {0};
+    int n = 3;
+    int[] c = {1};
     towerOfHanoi(n, 1, 3, 2, c);
-    log.info("CNT : {}", c[0]);
+    log.info("Required No. of moves : {}", c[0]);
   }
 
   /**
@@ -20,18 +20,15 @@ public class TowerOfHanoi {
    */
   private static void towerOfHanoi(int n, int source, int destination, int helper, int[] c) {
     // base case:
-    // only one disc is present :
-    if (1 == n) {
-      log.info("Move disk {} from {} to {}", n, source, destination);
+    if (n == 1) {
+      log.info("move disc {} from {} to {}", n, source, destination);
       c[0]++;
       return;
     }
-    ///  process the n-1 discs:
     towerOfHanoi(n - 1, source, helper, destination, c);
-    // now only the nth disc needs to be moved from the src to dest:
-    log.info("Move disk {} from {} to {}", n, source, destination);
+    // after the above step, the n-1 discs are sorted,
+    log.info("move disc : {}, from {} to {}", n, source, destination);
     c[0]++;
-    // now we need to take care of the remaining n-1 discs...
     towerOfHanoi(n - 1, helper, destination, source, c);
   }
 }
