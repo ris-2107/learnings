ae44337d587a3eb206db328687fae3e24a4b7292
Rishabh Sinha <rsinha@tekion.com>
2025-01-12 14:15:12 +0530

    12-jan-2025[0.0]

--- src/main/java/com/ris/learnings/dsa/recursion/KthSymbolInGrammar.java
+++ src/main/java/com/ris/learnings/dsa/recursion/KthSymbolInGrammar.java
@@ -5,14 +5,13 @@ public class KthSymbolInGrammar {
   public static void main(String[] args) {}
 
   private static int grammar(int n, int k) {
-
     // base condition:
     if (n == 1 && k == 1) {
       return 0;
     }
+
     // parent position:
     int parent = (k + 1) / 2;
-
     int parentValue = grammar(n - 1, parent);
 
     if (k % 2 == 0) {
