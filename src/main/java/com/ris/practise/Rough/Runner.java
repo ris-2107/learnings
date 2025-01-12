cd5696d28fb529559705c17168dba172aba76c74
Rishabh Sinha <rsinha@tekion.com>
2025-01-13 01:03:30 +0530

    13-jan-2025[0.0]

--- src/main/java/com/ris/practise/Rough/Runner.java
+++ src/main/java/com/ris/practise/Rough/Runner.java
@@ -1,7 +1,40 @@
 package com.ris.practise.Rough;
 
-public class Runner {
-    public static void main(String[] args) {
+import java.util.ArrayList;
 
+public class Runner {
+  public static void main(String[] args) {
+    ArrayList<Integer> ar = new ArrayList<>();
+    ar.add(1);
+    ar.add(2);
+    ar.add(3);
+    ar.add(4);
+    ar.add(5);
+    reverseArray(ar);
+    ar.forEach(System.out::println);
+  }
+
+  private static void reverseArray(ArrayList<Integer> arr) {
+    // base case:
+    if (arr.size() == 1) {
+      return;
     }
+
+    int poppedEle = arr.get(arr.size() - 1);
+    arr.remove(arr.size() - 1);
+    reverseArray(arr);
+    insertEle(arr, poppedEle);
+  }
+
+  private static void insertEle(ArrayList<Integer> arr, int element) {
+    // base case:
+    if (arr.isEmpty()) {
+      arr.add(element);
+      return;
+    }
+    int poppedElement = arr.get(arr.size() - 1);
+    arr.remove(arr.size() - 1);
+    insertEle(arr, element);
+    arr.add(poppedElement);
+  }
 }
