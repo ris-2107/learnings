65591f77b1c97b69ae8a7e19807aa72f7529a5a0
Rishabh Sinha <rsinha@tekion.com>
2025-01-18 19:33:42 +0530

    18-jan-2025[0.0]

---
+++ src/main/java/com/ris/learnings/dsa/dp/BooleanParenthesesRecursive.java
@@ -0,0 +1,24 @@
+package com.ris.learnings.dsa.dp;
+
+public class BooleanParenthesesRecursive {
+  // given a string :
+  public static void main(String[] args) {
+    String s = "kl";
+  }
+
+  private static boolean booleanP(String s, int i, int j, boolean flag) {
+    if (i > j) return false;
+    // single character:
+    if (i == j) {
+      if (flag) {
+        if (s.charAt(i) == 'T') return true;
+      } else if (s.charAt(i) == 'F') return true;
+    }
+    for (int k = i + 1; k < j; k = k + 2) {
+      boolean leftTrue = booleanP(s, i, k - 1, true);
+      boolean leftFalse = booleanP(s, i, k - 1, false);
+      boolean rightTrue = booleanP(s, k + 1, j, true);
+      boolean rightFalse = booleanP(s, k + 1, j, false);
+    }
+  }
+}
---
+++ src/main/java/com/ris/learnings/dsa/dp/PalindromePartitionBottomUpOptimised.java
@@ -0,0 +1,43 @@
+package com.ris.learnings.dsa.dp;
+
+public class PalindromePartitionBottomUpOptimised {
+  public static void main(String[] args) {}
+
+  private static int pp(String s, int i, int j, int[][] DP) {
+    if (i >= j) return 0;
+    if (isPalindrome(s, i, j)) {
+      return 0;
+    }
+    if (DP[i][j] != -1) return DP[i][j];
+    int MIN = Integer.MAX_VALUE;
+    for (int k = 0; k < j; k++) {
+      int left;
+      int right;
+      if (DP[i][k] != -1) {
+        left = DP[i][k];
+      } else {
+        left = pp(s, i, k, DP);
+        DP[i][k] = left;
+      }
+      if (DP[k + 1][j] != -1) {
+        right = DP[k + 1][j];
+      } else {
+        right = pp(s, k + 1, j, DP);
+        DP[k + 1][j] = right;
+      }
+      int selfCost = 1;
+      int totalCost = left + right + selfCost;
+      MIN = Math.min(MIN, totalCost);
+    }
+    return MIN;
+  }
+
+  private static boolean isPalindrome(String s, int i, int j) {
+    while (i < j) {
+      if (s.charAt(i) != s.charAt(j)) return false;
+      i++;
+      j--;
+    }
+    return true;
+  }
+}
