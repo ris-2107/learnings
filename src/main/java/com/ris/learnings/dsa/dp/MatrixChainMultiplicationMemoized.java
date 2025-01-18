1511419e79226cb8cd9199239af938c65022629e
Rishabh Sinha <rsinha@tekion.com>
2025-01-18 16:12:19 +0530

    18-jan-2025[0.0]

--- src/main/java/com/ris/learnings/dsa/dp/MatrixChainMultiplicationMemoized.java
+++ src/main/java/com/ris/learnings/dsa/dp/MatrixChainMultiplicationMemoized.java
@@ -1,12 +1,11 @@
 package com.ris.learnings.dsa.dp;
 
-import lombok.extern.slf4j.Slf4j;
-
 import java.util.Arrays;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class MatrixChainMultiplicationMemoized {
-      public static void main(String[] args) {
+  public static void main(String[] args) {
     int[] arr = {40, 20, 30, 10, 30};
     int n = arr.length;
 
@@ -14,7 +13,7 @@ public class MatrixChainMultiplicationMemoized {
     for (int i = 0; i < DP.length; i++) {
       Arrays.fill(DP[i], -1);
     }
-    int ans = mcm(arr, 1,n-1,DP);
+    int ans = mcm(arr, 1, n - 1, DP);
     log.info("ANS : {}", ans);
   }
 
@@ -22,14 +21,14 @@ public class MatrixChainMultiplicationMemoized {
     if (i >= j) {
       return 0;
     }
-        int MIN = Integer.MAX_VALUE;
+    int MIN = Integer.MAX_VALUE;
     if (DP[i][j] != -1) return DP[i][j];
-    for (int k=i;k<=j-1;k++){
-      int leftSubArray = mcm(arr,i,k, DP);
-      int rightSubArray = mcm(arr,k+1,j, DP);
-      int leftXRightMultiplicationCost = arr[i-1]*arr[k]*arr[j];
-      int totalCost = leftXRightMultiplicationCost + leftSubArray+rightSubArray;
-      MIN =Math.min(MIN, totalCost);
+    for (int k = i; k <= j - 1; k++) {
+      int leftSubArray = mcm(arr, i, k, DP);
+      int rightSubArray = mcm(arr, k + 1, j, DP);
+      int leftXRightMultiplicationCost = arr[i - 1] * arr[k] * arr[j];
+      int totalCost = leftXRightMultiplicationCost + leftSubArray + rightSubArray;
+      MIN = Math.min(MIN, totalCost);
     }
     return MIN;
   }
