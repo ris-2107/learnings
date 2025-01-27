e64d5adcd178d65e4df7ecbc092fadb32982e803
Rishabh Sinha <rsinha@tekion.com>
2025-01-27 23:07:42 +0530

    27-jan-2025[graph][0.0]

--- src/main/java/com/ris/Rough/Runner.java
+++ src/main/java/com/ris/Rough/Runner.java
@@ -1,7 +1,6 @@
 package com.ris.Rough;
 
-import java.util.Arrays;
-import java.util.Map;
+import java.util.*;
 
 public class Runner {
   static int[][] t = new int[1002][1002];
@@ -34,6 +33,24 @@ public class Runner {
       return t[n][leftCapacity] = Math.max(includeCase, excludeCase);
     } else {
       return knapsack(wt, val, leftCapacity, n - 1);
+    }
+  }
+
+  private static void bfs(Map<Integer, ArrayList<Integer>> graph) {
+    Queue<Integer> q = new LinkedList<>();
+    HashSet<Integer> visited = new HashSet<>();
+    final Integer firstNode = graph.keySet().iterator().next();
+    q.add(firstNode);
+    visited.add(firstNode);
+    while (!q.isEmpty()) {
+      int polledEle = q.poll();
+      System.out.println(polledEle);
+      for (int neighbour : graph.get(polledEle)) {
+        if (!visited.contains(neighbour)) {
+          q.add(neighbour);
+          visited.add(neighbour);
+        }
+      }
     }
   }
 }
---
+++ src/main/java/com/ris/Rough/RunnerTemplates/Runner1.java
@@ -0,0 +1,12 @@
+package com.ris.Rough.RunnerTemplates;
+
+public class Runner1 {
+  public static void main(String[] args) {
+    solve(1);
+  }
+
+  private static int solve(int x) {
+    System.out.println("Ans" + x);
+    return x;
+  }
+}
