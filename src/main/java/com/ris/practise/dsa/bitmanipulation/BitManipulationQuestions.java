939fc7f3bb952b05ceb7fa5a30d0c6217d126f57
Rishabh Sinha <rsinha@tekion.com>
2025-07-04 02:48:05 +0530

    [Add][bit manipulation]

--- src/main/java/com/ris/practise/dsa/bitmanipulation/BitManipulationQuestions.java
+++ src/main/java/com/ris/practise/dsa/bitmanipulation/BitManipulationQuestions.java
@@ -135,8 +135,8 @@ public class BitManipulationQuestions {
             int ans = 0;
             for (int i = 0; i < 32; i++) {
                 int count = 0;
-                for (int j = 0; j < nums.length; j++) {
-                    if ((nums[j] & (1 << i)) != 0) {
+                for (int num : nums) {
+                    if ((num & (1 << i)) != 0) {
                         count++;
                     }
                 }
