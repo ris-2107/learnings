2e58f80b1da8741a314155f9630d42a143cc4528
Rishabh Sinha <rsinha@tekion.com>
2025-07-04 02:45:32 +0530

    [Add][bit manipulation]

--- src/main/java/com/ris/practise/dsa/bitmanipulation/BitManipulationQuestions.java
+++ src/main/java/com/ris/practise/dsa/bitmanipulation/BitManipulationQuestions.java
@@ -1,5 +1,8 @@
 package com.ris.practise.dsa.bitmanipulation;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class BitManipulationQuestions {
 
     public static class Swap2Nos {
@@ -73,6 +76,76 @@ public class BitManipulationQuestions {
             System.out.println("Initial Number: " + Integer.toBinaryString(n));
             System.out.println("After Toggling ith bit: " + Integer.toBinaryString(ans));
 
+        }
+    }
+
+    // Application Questions
+
+    public static class minFlipsRequiredToConvertANo {
+        public static void main(String[] args) {
+            int start = 10;
+            int goal = 7;
+            int xor = start ^ goal; // xor variable 's 1 bits are the bits that need to be flipped
+            int c = 0;
+            for (int i = 0; i <= 31; i++) {
+                if ((xor & (1 << i)) != 0) {
+                    c++;
+                }
+            }
+            System.out.println("Min flips required from start " + start + " to goal " + goal + " is: " + c);
+        }
+    }
+
+    public static class printSubSets {
+        public static void main(String[] args) {
+            int[] nums = {1, 2, 3};
+            ArrayList<ArrayList<Integer>> subsets = new ArrayList<>();
+            int maxPossibleSubsetsCount = (1 << nums.length);
+            for (int i = 0; i < maxPossibleSubsetsCount; i++) {
+                ArrayList<Integer> subset = new ArrayList<>();
+                for (int j = 0; j < nums.length; j++) {
+                    if ((i & (1 << j)) != 0) { // if jth bit is set in i, means we need to take that element:
+                        subset.add(nums[j]);
+                    }
+                }
+                subsets.add(subset);
+            }
+            System.out.println(subsets);
+        }
+
+
+    }
+
+    public static class singleNumber1 {
+        static final int[] nums = {1, 1, 2, 4, 4, 5, 5, 6, 6};
+
+        public static void main(String[] args) {
+            int ans = 0;
+            for (int ele : nums) {
+                ans = ans ^ ele;
+            }
+            System.out.println("The single number is: " + ans);
+        }
+    }
+
+    public static class singleNumber2 {
+        static int[] nums = {1, 1, 1, 2, 4, 5, 6, 4, 4, 5, 5, 6, 6};
+
+        public static void main(String[] args) {
+            int ans = 0;
+            for (int i = 0; i < 32; i++) {
+                int count = 0;
+                for (int j = 0; j < nums.length; j++) {
+                    if ((nums[j] & (1 << i)) != 0) {
+                        count++;
+                    }
+                }
+                if (count % 3 == 1) {
+                    // set the ith bit in ans:
+                    ans = ans | (1 << i);
+                }
+            }
+            System.out.println("The single number is: " + ans);
         }
     }
 
