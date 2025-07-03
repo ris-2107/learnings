704e5b2a5e956366468577b0e6bb8aeabc898220
Rishabh Sinha <rsinha@tekion.com>
2025-07-03 21:50:41 +0530

    [Add][test commit][0]

---
+++ src/main/java/com/ris/practise/Rough/MCMPractise1.java
@@ -0,0 +1,13 @@
+package com.ris.practise.Rough;
+
+public class MCMPractise1 {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 30};
+        System.out.println(mcm(arr, 1, arr.length - 1));
+    }
+
+    private static int mcm(int[] arr, int i, int j) {
+        if(i>=j) return -1;
+     return 0;
+    }
+}
